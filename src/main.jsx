85dd2075e0937a4414af2427641092d92f86eeb7
Abhishek Sarathe <abhisheksarathe2917@gmail.com>
2026-04-09 08:03:15 +0530

    hello

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,4 @@
-<<<<<<< HEAD
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./routes";
 
-createRoot(document.getElementById("root")).render(
-  <>
-    <App />
-  </>
-);
-=======
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
@@ -20,4 +9,4 @@ createRoot(document.getElementById('root')).render(
     <App />
   </>
 )
->>>>>>> 813a288f9424fdbf7bcc2f46c6aa1de8caeeda53
+//  813a288f9424fdbf7bcc2f46c6aa1de8caeeda53
